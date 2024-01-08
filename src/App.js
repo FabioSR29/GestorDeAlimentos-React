5cbee9708f1536e623022b97c92526228e6ebd1a
FabioSR29 <fabiosalazarruiz@gmail.com.com>
2024-01-08 16:50:46 -0600

    cambios

--- src/App.js
+++ src/App.js
@@ -9,6 +9,8 @@ function App() {
   const [MostrarModalAgregar, setMostrarModalAgregar] = useState(false);
   const [ProductoAEditar, setProductoAEditar] = useState();
   const [MostrarFiltros, setMostrarFiltros] = useState();
+  const [ProductoABorrar, setProductoABorrar] = useState();
+  const [Borrar, setBorrar] = useState(false)
 
   function AgregueProducto(NuevoProducto) {
     setListaFiltrada([...ListaDeAlimentos, NuevoProducto]);
@@ -19,7 +21,8 @@ function App() {
     const NuevaLista = ListaDeAlimentos.filter(producto => producto.id !== Producto.id);
     setListaFiltrada([...NuevaLista]);
     setListaDeAlimentos([...NuevaLista]);
-
+    setBorrar(false)
+    alert("Alimento eliminado!")
   }
   function EditeElProducto(ListaActualizada) {
     setListaFiltrada(ListaActualizada);
@@ -36,6 +39,15 @@ function App() {
   }
   function CiereModalAEditar() {
     setMostrarModalEditar(false);
+
+  }
+  function MuestreModalBorrar(Producto) {
+    setProductoABorrar(Producto)
+    setBorrar(true)
+
+  }
+  function CiereModalBorrar() {
+    setBorrar(false)
 
   }
   function FiltrePorBebidas() {
@@ -144,7 +156,7 @@ function App() {
 
             <div key={index}>
 
-              <CartaDeProducto Producto={item} ListaDeAlimentos={ListaDeAlimentos} BorreElProducto={BorreElProducto} EditeElProducto={EditeElProducto} MuestreModalEditar={MuestreModalEditar} index={index}></CartaDeProducto>
+              <CartaDeProducto Producto={item} ListaDeAlimentos={ListaDeAlimentos} MuestreModalBorrar={MuestreModalBorrar} EditeElProducto={EditeElProducto} MuestreModalEditar={MuestreModalEditar} index={index}></CartaDeProducto>
 
 
             </div>
@@ -160,7 +172,17 @@ function App() {
       {MostrarModalAgregar &&
         <FormularioParaAgregarProducto ListaDeAlimentos={ListaDeAlimentos} AgregueProducto={AgregueProducto} CamabieElEstadoDelModalAgregar={CamabieElEstadoDelModalAgregar}></FormularioParaAgregarProducto>
       }
-
+      {Borrar &&
+        <Modal>
+          <div className='Formulario'>
+            <p>¿Estás seguro de borrar?</p>
+            <div className='FormularioBotones'>
+              <button onClick={() => { BorreElProducto(ProductoABorrar) }} className='FormularioBoton'>Si</button>
+              <button onClick={() => { CiereModalBorrar() }}className='FormularioBoton colorRojo'>No</button>
+            </div>
+          </div>
+        </Modal>
+      }
 
     </div>
   );
@@ -257,14 +279,14 @@ function FormularioParaEditarProducto(Propiedades) {
     <div>
       <Modal>
         <form onSubmit={EditeElElementoEnLaLista} className='Formulario'>
-          <label htmlFor='NombreDeLaPersona'>Agregue el nombre:</label>
+          <label htmlFor='NombreDeLaPersona'>Agregue el Nombre:</label>
           <input type='text' name='Nombre' id='NombreDeLaPersona' onChange={CargarLosNuevosValoresDeNombre} value={NuevoNombre} required></input>
-          <label htmlFor='TipoDeAlimento'>Tipo</label>
+          <label htmlFor='TipoDeAlimento'>Agregue el Tipo:</label>
           <select type='text' name='Tipo' id='TipoDeAlimento' onChange={CargarLosnuevosValoresDeTipo} value={NuevoTipo}>
             <option>Comida</option>
             <option>Bebida</option>
           </select>
-          <label htmlFor='CantidadEnBodega'>Cantidad en bodega</label>
+          <label htmlFor='CantidadEnBodega'>Indique la Cantidad en Bodega:</label>
           <input type='number' name='Cantidad' id='CantidadEnBodega' onChange={CargarLosNuevosValoresDeCantidad} value={NuevaCantidad} required></input>
           <div className='FormularioBotones'>
             <button onClick={Propiedades.CiereModalAEditar} className='FormularioBoton colorRojo'>Cancelar</button>
@@ -278,7 +300,9 @@ function FormularioParaEditarProducto(Propiedades) {
 
 function CartaDeProducto(Propiedades) {
 
-  const { Producto, index } = Propiedades;
+  const { Producto } = Propiedades;
+
+
 
   return (
     <div className='Productos'>
@@ -291,7 +315,7 @@ function CartaDeProducto(Propiedades) {
       </div>
 
       <div className='ProductoBotones'>
-        <button onClick={() => { Propiedades.BorreElProducto(Producto) }} className='btnBorrar'>
+        <button onClick={() => { Propiedades.MuestreModalBorrar(Producto) }} className='btnBorrar'>
           <svg viewBox="0 0 448 512" className="svgIcon"><path d="M135.2 17.7L128 32H32C14.3 32 0 46.3 0 64S14.3 96 32 96H416c17.7 0 32-14.3 32-32s-14.3-32-32-32H320l-7.2-14.3C307.4 6.8 296.3 0 284.2 0H163.8c-12.1 0-23.2 6.8-28.6 17.7zM416 128H32L53.2 467c1.6 25.3 22.6 45 47.9 45H346.9c25.3 0 46.3-19.7 47.9-45L416 128z"></path></svg>
         </button>
 
@@ -321,6 +345,7 @@ function TipoAlimento(Propiedades) {
       </div>);
   }
 }
+
 
 function Modal({ children }) {
   return (
